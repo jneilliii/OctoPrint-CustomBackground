5abfdd513cd7cab7aca378dbb84d7cefa13669d2
jneilliii <jneilliii+github@gmail.com>
2017-09-16 20:16:25 -0400

    adding route hook to move uploaded file into plugin data folder.

--- octoprint_custombackground/__init__.py
+++ octoprint_custombackground/__init__.py
@@ -29,16 +29,28 @@ class custombackground(octoprint.plugin.AssetPlugin,
 			)
 		)
 	
-	##-- Image upload preprocessor hook	
+	##~~ Image upload preprocessor hook	
 	def custombackgroundupload(self, path, file_object, links=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
 		img_extensions = [".jpg", ".bmp", ".png", ".gif"]
 		name, extension = os.path.splitext(file_object.filename)
 		if extension in img_extensions:
 			self._logger.info("Setting background url for " + path)
+			self._logger.info(self.get_plugin_data_folder())
 			self._settings.set(["background_url"],"/downloads/files/local/" + path)
 			self._settings.save()
 			self._plugin_manager.send_plugin_message(self._identifier, dict(type="reload"))
 		return file_object
+		
+	##~~ Routes hook
+	def route_hook(self, server_routes, *args, **kwargs):
+		from octoprint.server.util.tornado import LargeResponseHandler, UrlProxyHandler, path_validation_factory
+		from octoprint.util import is_hidden_path
+
+		return [
+				(r"/background/(.*)", LargeResponseHandler, dict(path=self.get_plugin_data_folder(),
+																as_attachment=True,
+																path_validation=path_validation_factory(lambda path: not is_hidden_path(path),status_code=404)))
+				]
 		
 	##~~ Softwareupdate hook
 	def get_version(self):
@@ -71,5 +83,6 @@ def __plugin_load__():
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
 		"octoprint.filemanager.extension_tree": __plugin_implementation__.get_extension_tree,
-		"octoprint.filemanager.preprocessor": __plugin_implementation__.custombackgroundupload
+		"octoprint.filemanager.preprocessor": __plugin_implementation__.custombackgroundupload,
+		"octoprint.server.http.routes": __plugin_implementation__.route_hook
 	}

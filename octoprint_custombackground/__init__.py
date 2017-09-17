c7a50bb0355832206e35eae93827e70c99c08820
jneilliii <jneilliii+github@gmail.com>
2017-09-16 23:51:56 -0400

    test

--- octoprint_custombackground/__init__.py
+++ octoprint_custombackground/__init__.py
@@ -36,11 +36,11 @@ class custombackground(octoprint.plugin.AssetPlugin,
 		if extension in img_extensions:
 			self._logger.info("Setting background url for " + path)
 			#file_object.save(self.get_plugin_data_folder() + "/uploaded" + extension)
+			octoprint.filemanager.util.StreamWrapper(self.get_plugin_data_folder() + "/uploaded" + extension, file_object.stream()).save(self.get_plugin_data_folder() + "/uploaded" + extension)
 			self._logger.info(self.get_plugin_data_folder() + "/uploaded" + extension)
 			self._settings.set(["background_url"],"/plugin/custombackground/uploaded" + extension)
 			self._settings.save()
 			self._plugin_manager.send_plugin_message(self._identifier, dict(type="reload"))
-			return octoprint.filemanager.util.StreamWrapper(self.get_plugin_data_folder() + "/uploaded" + extension, file_object.stream())
 		return file_object
 		
 	##~~ Routes hook

ab9a0b1207e9cbfb4f0696d7639d54f74d3fadf1
jneilliii <jneilliii+github@gmail.com>
2017-12-10 02:07:07 -0500

    Update custombackground.js

--- octoprint_custombackground/static/js/custombackground.js
+++ octoprint_custombackground/static/js/custombackground.js
@@ -26,7 +26,7 @@ $(function() {
         }
 		
 		self.onAfterBinding = function() {
-			$("#temperature-graph").css({"background-image":"url('" + self.settings.settings.plugins.custombackground.background_url() + "')","background-size":self.settings.settings.plugins.custombackground.fillMethod(),"background-position":self.settings.settings.plugins.custombackground.position()});
+			$("#temperature-graph").css({"background-image":"url('" + window.location.href.replace(/[^/]*$/, '') + self.settings.settings.plugins.custombackground.background_url() + "')","background-size":self.settings.settings.plugins.custombackground.fillMethod(),"background-position":self.settings.settings.plugins.custombackground.position()});
 		}
 		
 		self.onEventSettingsUpdated = function (payload) {            

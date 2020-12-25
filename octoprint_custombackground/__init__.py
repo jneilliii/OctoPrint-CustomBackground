0ceb20a3797268aba1ee7904ae4100df24dc204c
jneilliii <jneilliii+github@gmail.com>
2020-12-25 02:07:11 -0500

    1.0.0 (#36)
    * add all uppercase extension options
    * add support for jpeg and webp extensions
    * cache busters in thumbnail url based on upload timestamp
    * pre-package some generic grayscale image and jneilliii fun image
    * add support for release channels available in OctoPrint 1.5.0
    * add text and grid color options
    * move out of beta versioning

--- octoprint_custombackground/__init__.py
+++ octoprint_custombackground/__init__.py
@@ -4,20 +4,34 @@ import octoprint.plugin
 import os
 import octoprint.filemanager
 import octoprint.filemanager.util
+import datetime
 
 class custombackground(octoprint.plugin.AssetPlugin,
 				octoprint.plugin.TemplatePlugin,
                 octoprint.plugin.SettingsPlugin):
 	
-	##-- AssetPlugin hooks
+	##-- AssetPlugin mixin
 	def get_assets(self):
 		return dict(js=["js/custombackground.js"])
-		
-	##-- Settings hooks
+
+	##-- Settings mixin
 	def get_settings_defaults(self):
-		return dict(background_url="/static/img/graph-background.png",icon_url="/static/img/tentacle-20x20.png",fillMethod="cover",position="center center")
-	
-	##-- Template hooks
+		return dict(background_url="/static/img/graph-background.png", icon_url="/static/img/tentacle-20x20.png", fillMethod="cover", position="center center", uploaded_url="", axes_text_color="", tick_color="", temp_line_colors="")
+
+	def get_settings_version(self):
+		return 1
+		
+	def on_settings_migrate(self, target, current=None):
+		if current is None or current < 1:
+			migration_url = self._settings.get(["background_url"])
+			self._logger.info(migration_url)
+			if migration_url.startswith("/plugin/custombackground/uploaded"):
+				new_background_url = migration_url.replace("/plugin/custombackground/uploaded", "/plugin/custombackground/custom/uploaded")
+				self._logger.info(new_background_url)
+				self._settings.set(["background_url"], new_background_url)
+				self._settings.set(["uploaded_url"], new_background_url)
+
+	##-- Template mixin
 	def get_template_configs(self):
 		return [dict(type="settings",custom_bindings=True)]
 
@@ -25,29 +39,31 @@ class custombackground(octoprint.plugin.AssetPlugin,
 	def get_extension_tree(self, *args, **kwargs):
 		return dict(
 			machinecode=dict(
-				custombackground=["jpg", "bmp", "png", "gif"]
+				custombackground=["jpg", "bmp", "png", "gif", "jpeg", "webp", "JPG", "BMP", "PNG", "GIF", "JPEG", "WEBP"]
 			)
 		)
 	
 	##~~ Image upload preprocessor hook	
 	def custombackgroundupload(self, path, file_object, links=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
-		img_extensions = [".jpg", ".bmp", ".png", ".gif"]
+		img_extension_tree = self.get_extension_tree()
+		img_extensions = img_extension_tree.get("machinecode").get("custombackground")
 		name, extension = os.path.splitext(file_object.filename)
 		if name == "icon":
 			self._logger.info("Setting icon url for " + path)
 			#file_object.save(self.get_plugin_data_folder() + "/uploaded" + extension)
 			octoprint.filemanager.util.StreamWrapper(self.get_plugin_data_folder() + "/icon" + extension, file_object.stream()).save(self.get_plugin_data_folder() + "/icon" + extension)
 			self._logger.info(self.get_plugin_data_folder() + "/icon" + extension)
-			self._settings.set(["icon_url"],"/plugin/custombackground/icon" + extension)
+			self._settings.set(["icon_url"], "/plugin/custombackground/custom/icon{}?{:%Y%m%d%H%M%S}".format(extension, datetime.datetime.now()))
 			self._settings.save()
 			self._plugin_manager.send_plugin_message(self._identifier, dict(type="reload"))
 			return file_object
-		if extension in img_extensions:
+		if extension.replace(".", "") in img_extensions:
 			self._logger.info("Setting background url for " + path)
-			#file_object.save(self.get_plugin_data_folder() + "/uploaded" + extension)
+			# file_object.save(self.get_plugin_data_folder() + "/uploaded" + extension)
 			octoprint.filemanager.util.StreamWrapper(self.get_plugin_data_folder() + "/uploaded" + extension, file_object.stream()).save(self.get_plugin_data_folder() + "/uploaded" + extension)
 			self._logger.info(self.get_plugin_data_folder() + "/uploaded" + extension)
-			self._settings.set(["background_url"],"/plugin/custombackground/uploaded" + extension)
+			self._settings.set(["background_url"], "/plugin/custombackground/custom/uploaded{}?{:%Y%m%d%H%M%S}".format(extension, datetime.datetime.now()))
+			self._settings.set(["uploaded_url"], "/plugin/custombackground/custom/uploaded{}?{:%Y%m%d%H%M%S}".format(extension, datetime.datetime.now()))
 			self._settings.save()
 			self._plugin_manager.send_plugin_message(self._identifier, dict(type="reload"))
 		return file_object
@@ -57,7 +73,7 @@ class custombackground(octoprint.plugin.AssetPlugin,
 		from octoprint.server.util.tornado import LargeResponseHandler, UrlProxyHandler, path_validation_factory
 		from octoprint.util import is_hidden_path
 		return [
-				(r"/(.*)", LargeResponseHandler, dict(path=self.get_plugin_data_folder(),
+				(r"/custom/(.*)", LargeResponseHandler, dict(path=self.get_plugin_data_folder(),
 																as_attachment=True,
 																path_validation=path_validation_factory(lambda path: not is_hidden_path(path),status_code=404)))
 				]
@@ -77,6 +93,16 @@ class custombackground(octoprint.plugin.AssetPlugin,
 				user="jneilliii",
 				repo="OctoPrint-CustomBackground",
 				current=self._plugin_version,
+				stable_branch=dict(
+					name="Stable", branch="master", comittish=["master"]
+				),
+				prerelease_branches=[
+					dict(
+						name="Release Candidate",
+						branch="rc",
+						comittish=["rc", "master"],
+					)
+				],
 
 				# update method: pip
 				pip="https://github.com/jneilliii/OctoPrint-CustomBackground/archive/{target_version}.zip"
--- octoprint_custombackground/static/js/custombackground.js
+++ octoprint_custombackground/static/js/custombackground.js
@@ -1,9 +1,10 @@
 $(function() {
-    function custombackgroundViewModel(parameters) {
-        var self = this;
-		
+	function custombackgroundViewModel(parameters) {
+		var self = this;
+
 		self.settings = parameters[0];
-		
+		self.temperatureViewModel = parameters[1];
+
 		self.background_url = ko.observable();
 		self.icon_url = ko.observable();
 		self.fillMethod = ko.observable();
@@ -19,57 +20,126 @@ $(function() {
 					}
 				]);
 		self.position = ko.observable();
-		
+		self.selectedBundledImage = ko.observable('')
+
 		self.onBeforeBinding = function() {
-            self.background_url(self.settings.settings.plugins.custombackground.background_url());
-            self.icon_url(self.settings.settings.plugins.custombackground.icon_url());
+			self.bundledImages = ko.observableArray([{
+							name : 'OctoPrint',
+							path : '/static/img/graph-background.png'
+						}, {
+							name : 'Custom',
+							path : self.settings.settings.plugins.custombackground.uploaded_url()
+						}, {
+							name : 'jneilliii',
+							path : '/plugin/custombackground/static/img/jneilliii.png'
+						}, {
+							name : 'Transparent',
+							path : '/plugin/custombackground/static/img/ocotoprint-transparent.png'
+						}, {
+							name : '10% gray',
+							path : '/plugin/custombackground/static/img/ocotoprint-gray-10.png'
+						}
+					]);
+			self.background_url(self.settings.settings.plugins.custombackground.background_url());
+			self.selectedBundledImage(self.settings.settings.plugins.custombackground.background_url());
+			self.icon_url(self.settings.settings.plugins.custombackground.icon_url());
 			self.fillMethod(self.settings.settings.plugins.custombackground.fillMethod());
 			self.position(self.settings.settings.plugins.custombackground.position());
-        }
-		
+		}
+
 		self.onAfterBinding = function() {
 			$("#temperature-graph").css({"background-image":"url('" + window.location.pathname.replace(/\/$/, '') + self.settings.settings.plugins.custombackground.background_url() + "')","background-size":self.settings.settings.plugins.custombackground.fillMethod(),"background-position":self.settings.settings.plugins.custombackground.position()});
 			$("#navbar .navbar-inner .brand span").css({"background-image":"url('" + window.location.pathname.replace(/\/$/, '') + self.settings.settings.plugins.custombackground.icon_url() + "')"});
 		}
-		
+
+		self.onSettingsSaved = function() {
+			if(self.settings.settings.plugins.custombackground.background_url() == '/static/img/graph-background.png' || self.settings.settings.plugins.custombackground.background_url() == '/plugin/custombackground/static/img/jneilliii.png'){
+				self.settings.settings.plugins.custombackground.icon_url('/static/img/tentacle-20x20.png');
+				self.settings.settings.plugins.custombackground.fillMethod('auto');
+				self.settings.settings.plugins.custombackground.position('center center');
+			}
+		}
+
 		self.onEventSettingsUpdated = function (payload) {
-            self.background_url(self.settings.settings.plugins.custombackground.background_url());
-            self.icon_url(self.settings.settings.plugins.custombackground.icon_url());
+			self.background_url(self.settings.settings.plugins.custombackground.background_url());
+			self.icon_url(self.settings.settings.plugins.custombackground.icon_url());
+			self.bundledImages = ko.observableArray([{
+							name : 'OctoPrint',
+							path : '/static/img/graph-background.png'
+						}, {
+							name : 'Custom',
+							path : self.settings.settings.plugins.custombackground.uploaded_url()
+						}, {
+							name : 'jneilliii',
+							path : '/plugin/custombackground/static/img/jneilliii.png'
+						}, {
+							name : 'Transparent',
+							path : '/plugin/custombackground/static/img/ocotoprint-transparent.png'
+						}, {
+							name : '10% gray',
+							path : '/plugin/custombackground/static/img/ocotoprint-gray-10.png'
+						}
+					]);
+
 			$("#temperature-graph").css({"background-image":"url('" + window.location.pathname.replace(/\/$/, '') + self.settings.settings.plugins.custombackground.background_url() + "')","background-size":self.settings.settings.plugins.custombackground.fillMethod(),"background-position":self.settings.settings.plugins.custombackground.position()});
 			$("#navbar .navbar-inner .brand span").css({"background-image":"url('" + window.location.pathname.replace(/\/$/, '') + self.settings.settings.plugins.custombackground.icon_url() + "')"});
-        }
-		
+		}
+
+		self._updateTempGraphOptions = function(){
+			if (self.temperatureViewModel.plot){
+				if(self.settings.settings.plugins.custombackground.axes_text_color() !== ""){
+					self.temperatureViewModel.plot.getOptions().xaxis.color = self.settings.settings.plugins.custombackground.axes_text_color();
+					self.temperatureViewModel.plot.getOptions().yaxis.color = self.settings.settings.plugins.custombackground.axes_text_color();
+					self.temperatureViewModel.plot.getOptions().xaxes[0].font = {color: self.settings.settings.plugins.custombackground.axes_text_color()};
+					self.temperatureViewModel.plot.getOptions().yaxes[0].font = {color: self.settings.settings.plugins.custombackground.axes_text_color()};
+				}
+				if(self.settings.settings.plugins.custombackground.tick_color() !== ""){
+					self.temperatureViewModel.plot.getOptions().grid.tickColor = self.settings.settings.plugins.custombackground.tick_color();
+					self.temperatureViewModel.plot.getOptions().yaxis.tickColor = self.settings.settings.plugins.custombackground.tick_color();
+					self.temperatureViewModel.plot.getOptions().xaxis.tickColor = self.settings.settings.plugins.custombackground.tick_color();
+					self.temperatureViewModel.plot.getOptions().xaxes[0].tickColor = self.settings.settings.plugins.custombackground.tick_color();
+					self.temperatureViewModel.plot.getOptions().yaxes[0].tickColor = self.settings.settings.plugins.custombackground.tick_color();
+					self.temperatureViewModel.plot.getOptions().grid.borderColor = self.settings.settings.plugins.custombackground.tick_color();
+					self.temperatureViewModel.plot.getOptions().grid.markingsColor = self.settings.settings.plugins.custombackground.tick_color();
+				}
+				if(self.settings.settings.plugins.custombackground.temp_line_colors() !== ""){
+					self.temperatureViewModel.plot.getOptions().colors = JSON.parse(self.settings.settings.plugins.custombackground.temp_line_colors());
+				}
+				console.log(self.temperatureViewModel.plot.getOptions());
+			}
+		}
+
+		self.onAfterTabChange = function(current_tab, previous_tab){
+			if (current_tab == "#temp"){
+				if(self.settings.settings.plugins.custombackground.axes_text_color() !== "" || self.settings.settings.plugins.custombackground.tick_color() || self.settings.settings.plugins.custombackground.temp_line_colors() !== "") {
+					self._updateTempGraphOptions();
+				}
+			}
+		}
+
 		self.onDataUpdaterPluginMessage = function(plugin, data) {
-            if (plugin != "custombackground") {
-                return;
-            }
-			
+			if (plugin != "custombackground") {
+				return;
+			}
+
 			if(data.type == "reload") {
 				window.location.reload(true);
 			}
 		}
-		
-		self.resetBackgrounds = function(){
-			self.settings.settings.plugins.custombackground.background_url('/static/img/graph-background.png');
-			self.settings.settings.plugins.custombackground.icon_url('/static/img/tentacle-20x20.png');
-			self.settings.settings.plugins.custombackground.fillMethod('auto');
-			self.settings.settings.plugins.custombackground.position('center center');
-			self.settings.saveData();
-		}
-    }
+	}
 
-    // This is how our plugin registers itself with the application, by adding some configuration
-    // information to the global variable OCTOPRINT_VIEWMODELS
-    ADDITIONAL_VIEWMODELS.push([
-        // This is the constructor to call for instantiating the plugin
-        custombackgroundViewModel,
+	// This is how our plugin registers itself with the application, by adding some configuration
+	// information to the global variable OCTOPRINT_VIEWMODELS
+	ADDITIONAL_VIEWMODELS.push([
+		// This is the constructor to call for instantiating the plugin
+		custombackgroundViewModel,
 
-        // This is a list of dependencies to inject into the plugin, the order which you request
-        // here is the order in which the dependencies will be injected into your view model upon
-        // instantiation via the parameters argument
-        ["settingsViewModel"],
+		// This is a list of dependencies to inject into the plugin, the order which you request
+		// here is the order in which the dependencies will be injected into your view model upon
+		// instantiation via the parameters argument
+		["settingsViewModel", "temperatureViewModel"],
 
-        // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#settings_plugin_custombackground_form"]
-    ]);
+		// Finally, this is the list of selectors for all elements we want this view model to be bound to.
+		["#settings_plugin_custombackground_form"]
+	]);
 });
